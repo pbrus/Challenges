976a21d49d53334d182791ac39532040252e51de
Przemysław Bruś <19304263+pbrus@users.noreply.github.com>
2022-10-11 12:18:01 +0200

    Remove inefficient scripts

--- get_middle.py
+++
@@ -1,8 +0,0 @@
-def get_middle(s):
-    if len(s)/2 != int(len(s)/2):
-        return print(s[int(len(s)/2)])
-    else:
-        return print(s[int((len(s)/2))-1:(int(len(s)/2))+1])
-
-st = "middle"
-get_middle(st)
